5a49ef1ad7e82c701f838cc279bd1b428a1628fd
bilalbarina <dev.barina@gmail.com>
2022-12-22 13:06:18 +0100

    Added password reset.

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+use Kreait\Firebase\Contract\Auth as FirebaseAuth;
 
 class PasswordResetLinkController extends Controller
 {
@@ -26,7 +27,7 @@ class PasswordResetLinkController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request, FirebaseAuth $auth)
     {
         $request->validate([
             'email' => ['required', 'email'],
@@ -35,13 +36,13 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        try {
+            $auth->sendPasswordResetLink($request->email);
+        } catch(\Throwable $e) {
+            back()->withInput($request->only('email'))
+                            ->withErrors(['email' => 'Invalid email.']);
+        }
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+        return back()->with('status', 'Email sent.');
     }
 }
